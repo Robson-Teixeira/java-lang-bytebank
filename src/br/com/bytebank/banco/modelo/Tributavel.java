a76516ad263885ce14adee6b7fe7985128c41755
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-11-26 23:28:27 -0300

    Organizando as classes com Pacotes: O melhor pacote

--- src/br/com/bytebank/banco/modelo/Tributavel.java
+++ src/br/com/bytebank/banco/modelo/Tributavel.java
@@ -1,10 +1,11 @@
 package br.com.bytebank.banco.modelo;
 // FQN - Full Qualified Name
-	// br (domínio de topo nacional na internet)
-	// com (domínio de topo comercial na internet)
-	// bytebank (nome empresa)
-	// banco (nome projeto)
-	// modelo (nome contexto arquivos)
+	// NOME_DO_SITE_AO_CONTRARIO.NOME_DO_PROJETO
+		// br (domínio de topo nacional na internet)
+		// com (domínio de topo comercial na internet)
+		// bytebank (nome empresa)
+		// banco (nome projeto)
+		// modelo (nome contexto arquivos)
 
 public interface Tributavel {
 
