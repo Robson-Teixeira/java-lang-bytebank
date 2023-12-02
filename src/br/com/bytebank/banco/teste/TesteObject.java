c92816c1a51431f952bdb7c52f3696ba23c8bcdf
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-12-01 23:58:48 -0300

    A classe Object: Usando a classe Object

---
+++ src/br/com/bytebank/banco/teste/TesteObject.java
@@ -0,0 +1,25 @@
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+
+public class TesteObject {
+
+	public static void main(String[] args) {
+
+		System.out.println("TesteObject");
+		System.out.println(2023);
+		System.out.println(true);
+
+		ContaCorrente contaCorrente = new ContaCorrente(1123, 1337);
+		Object contaPoupanca = new ContaPoupanca(1123, 1338);
+		Object cliente = new Cliente();
+
+		System.out.println(contaCorrente.toString());
+		System.out.println(contaPoupanca);
+		System.out.println(cliente);
+
+	}
+
+}
